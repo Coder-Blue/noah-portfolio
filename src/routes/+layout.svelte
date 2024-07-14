d5979474d603ec95a07aacf5b6ca02a135429ba8
Coder-Blue <trananhquan1009@gmail.com>
2024-07-14 21:44:50 +0700

    Optimizing the SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
   import Header from "../components/Header.svelte";
   import Footer from "../components/Footer.svelte";
   import "../app.css";
+
+  inject({ mode: dev ? "development" : "production" });
 
   let y;
   let innerWidth = 0;
